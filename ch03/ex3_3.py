32b8f89e26cc20b5b81816a4c2c18b0fe0b97599
Indrayana Rustandi <i_rustandi@yahoo.com>
2015-09-25 11:33:51 -0700

    Correct iter, compute contribVec

--- ch03/ex3_3.py
+++ ch03/ex3_3.py
@@ -11,20 +11,17 @@ stateValues = np.array([100, 0])
 
 transMat = np.array([[0.7, 0.3], [0.05, 0.95]])
 contribMat = np.array([[10.0, 30.0], [20.0, 5.0]])
+contribVec = np.sum(transMat * contribMat, axis=1)
 
 valueMat = np.zeros([numIter+1, 2])
 valueMat[0,] = stateValues
 
 for iter in range(0, numIter):
     # state 1
-    state1_1_val = transMat[0, 0] * (contribMat[0, 0] + gamma * valueMat[iter-1, 0])
-    state1_2_val = transMat[0, 1] * (contribMat[0, 1] + gamma * valueMat[iter-1, 1])
-    valueMat[iter+1,0] = state1_1_val + state1_2_val
+    valueMat[iter+1,0] = contribVec[0] + gamma * (transMat[0,0] * valueMat[iter,0] + transMat[0,1] * valueMat[iter,1])
 
     # state 2
-    state2_1_val = transMat[1, 0] * (contribMat[1, 0] + gamma * valueMat[iter-1, 0])
-    state2_2_val = transMat[1, 1] * (contribMat[1, 1] + gamma * valueMat[iter-1, 1])
-    valueMat[iter+1,1] = state2_1_val + state2_2_val
+    valueMat[iter+1,1] = contribVec[1] + gamma * (transMat[1,0] * valueMat[iter,0] + transMat[1,1] * valueMat[iter,1])
 
 plt.plot(valueMat[:,0])
 plt.show()
